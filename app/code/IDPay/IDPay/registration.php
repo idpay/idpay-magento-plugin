10308b88e00cf201ed1e21a19934314435d73281
Ali Jazayeri - Ubuntu <jaza.ali@gmail.com>
2018-11-12 15:55:52 +0330

    first commit

---
+++ app/code/IDPay/IDPay/registration.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * IDPay payment gateway
+ *
+ * @developer JMDMahdi
+ * @publisher IDPay
+ * @copyright (C) 2018 IDPay
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GPLv2 or later
+ *
+ * http://idpay.ir
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'IDPay_IDPay',
+    __DIR__
+);
